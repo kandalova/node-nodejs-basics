9c2c9247798323cac623215ac1c4be4c984b73c2
Leyla Kandalova <leyla_kandalova@epam.com>
2023-06-20 02:24:02 +0600

    feat: add cp function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,14 @@
+import { fork } from "node:child_process"
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const dirName = path.dirname(fileURLToPath(import.meta.url));
+    const scriptPath = path.resolve(dirName, 'files/script.js');
+
+    const childProcess = fork(scriptPath, args, { stdio: 'pipe' });
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
