07da0601fd13be9eea3b167ca5f0953f6d67af45
Leyla Kandalova <leyla_kandalova@epam.com>
2023-06-20 00:53:28 +0600

    feat: compress and decompress functions

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { createUnzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
 const decompress = async () => {
-    // Write your code here 
+    const dirName = path.dirname(fileURLToPath(import.meta.url));
+    const destName = path.resolve(dirName, 'files/fileToCompress.txt');
+    const srcName = path.resolve(dirName, 'files/archive.gz');
+
+    const gzip = createUnzip();
+    const srcStream = createReadStream(srcName);
+    const destStream = createWriteStream(destName);
+
+    pipeline(srcStream, gzip, destStream, (err) => {
+        if (err) {
+            console.error('An error occurred:', err);
+            process.exitCode = 1;
+        }
+    });
 };
 
 await decompress();
