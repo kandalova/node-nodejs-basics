274b51b9f60474a883c6b40db4a6ea437723cb28
Leyla Kandalova <leyla_kandalova@epam.com>
2023-06-20 00:05:29 +0600

    feat: add read, transform, write functions in streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,27 @@
+import {createReadStream} from "node:fs"
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const dirName = path.dirname(fileURLToPath(import.meta.url));
+    const srcName = path.resolve(dirName, 'files/fileToRead.txt');
+
+    const readerStream = createReadStream(srcName);
+    readerStream.setEncoding('UTF8');
+
+    readerStream.on('data', ((chunk) => {
+        process.stdout.write(chunk);
+    }));
+
+    readerStream.on('end', (() => {
+        console.log('');
+        console.log('File reading complete.');
+    }));
+
+    readerStream.on('error', ((err) => {
+        console.log(err);
+    }));
+
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
+import { createWriteStream } from "node:fs"
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
 const write = async () => {
-    // Write your code here 
+    const dirName = path.dirname(fileURLToPath(import.meta.url));
+    const srcName = path.resolve(dirName, 'files/fileToWrite.txt');
+
+    const writerStream = createWriteStream(srcName);
+
+    console.log('Write something to file. Press CTRL+C to exit');
+
+    process.stdin.pipe(writerStream);
+
+    writerStream.on('error', (error) => {
+        console.error('Error writing to file:', error);
+        writerStream.end();
+    });
 };
 
 await write();
