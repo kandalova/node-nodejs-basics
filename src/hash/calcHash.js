cbff7ee0c80681d0d5c8786e2f00b4d278838412
Leyla Kandalova <leyla_kandalova@epam.com>
2023-06-19 21:49:39 +0600

    feat: add calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { readFile } from 'node:fs';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const dirName = path.dirname(fileURLToPath(import.meta.url));
+    const srcName = path.resolve(dirName, 'files/fileToCalculateHashFor.txt');
+
+    const {
+        createHash,
+    } = await import('node:crypto');
+    const hash = createHash('sha256');
+
+    
+
+    readFile(srcName, 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        };
+        console.log(data);
+        hash.update(data);
+        console.log(hash.digest('hex'));
+    });   
 };
 
 await calculateHash();
