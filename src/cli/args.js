903c299265244f4c0d0646bf2432bf42fb828491
Leyla Kandalova <leyla_kandalova@epam.com>
2023-06-19 20:00:03 +0600

    feat: add parseArgs and parseEnv in cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const argLine = process.argv.slice(2).join(' ');
+    const propValueRegexp = /(--[\w|-]+) (\w+)/gm
+    const pairs = argLine.match(propValueRegexp)    
+    .map((pair)=>{
+        console.log(pair);
+        return pair.replace('--', '').replace(' ', ' is ');
+    });
+    console.log(pairs.join(', '));
 };
 
 parseArgs();
